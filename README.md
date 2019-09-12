a16523e9c746d358b8089054bccfc7ac4dc2206b
kamoii <>
2019-09-12 23:40:37 +0900

    log request

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # rin
 
+HTTP/HTTPS server for dev/debug. Will add feature as I need. For now it just prints Request headers.
+
 ## After changing project.yaml/stack.yaml
 
 To update rin.cabal:
@@ -11,5 +13,7 @@ Update nix files(pkgs.nix, rin.nix):
     stack-to-nix --output nix --stack-yaml stack.yaml
 
 ## Build static binary
+
+Remove symbolick link `result` if there is any before bulding.
 
     nix build -f nix/release-static.nix
--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Prelude()
 import Relude
+import qualified Data.CaseInsensitive as CI
+import qualified Network.HTTP.Types as H
+import qualified Network.Wai as W
+import qualified Network.Wai.Middleware.RequestLogger as WM
 import qualified Options.Applicative as O
+import qualified Network.Wai.Handler.Warp as Warp
 
 main :: IO ()
 main = do
   port <- O.execParser opts
-  print port
+  putTextLn $ "Listen on port: " <> show port
+  Warp.run port
+    . reqLogMiddleware
+    $ \req respond -> respond $ W.responseLBS H.status200 [] ""
+
+-- TODO: pretty printer / color
+-- with co-log
+reqLogMiddleware :: W.Middleware
+reqLogMiddleware app req respond = do
+  putBSLn $ W.requestMethod req <> " " <> W.rawPathInfo req <> " " <> show (W.httpVersion req)
+  for_ (W.requestHeaders req) $ \(name, value) -> putBSLn $ CI.original name <> ": " <> value
+  app req respond
+
+
 
 opt :: O.Parser Int
 opt = O.option O.auto
