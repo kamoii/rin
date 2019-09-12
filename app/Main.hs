7737e7c9625a6fafa5a1f42892948739be141eb0
kamoii <>
2019-09-12 18:19:47 +0900

    Port option

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
 module Main where
 
-import Lib
+import Prelude()
+import Relude
+import qualified Options.Applicative as O
 
 main :: IO ()
-main = someFunc
+main = do
+  port <- O.execParser opts
+  print port
+
+opt :: O.Parser Int
+opt = O.option O.auto
+  $ O.short 'p'
+  <> O.long "port"
+  <> O.value 8080
+  <> O.showDefault
+  <> O.help "Port number"
+  <> O.metavar "PORT"
+
+opts = O.info (opt <**> O.helper)
+  $ O.fullDesc
+  <> O.header "rin - HTTP/HTTPS server for dev/debug"
