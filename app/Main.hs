50e098e2844610c5f8f2db198f29b7f76e563df9
kamoii <>
2019-09-13 09:09:30 +0900

    Add timestamp

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,8 @@ module Main where
 
 import Prelude()
 import Relude
+import qualified Data.Text as T
+import qualified Chronos as Ch
 import qualified Data.CaseInsensitive as CI
 import qualified Network.HTTP.Types as H
 import qualified Network.Wai as W
@@ -19,13 +21,15 @@ main = do
     $ \req respond -> respond $ W.responseLBS H.status200 [] ""
 
 -- TODO: pretty printer / color
+-- query
 -- with co-log
 reqLogMiddleware :: W.Middleware
 reqLogMiddleware app req respond = do
+  putTextLn $ T.replicate 60 "-"
+  putTextLn =<< encodeTime <$> Ch.now
   putBSLn $ W.requestMethod req <> " " <> W.rawPathInfo req <> " " <> show (W.httpVersion req)
   for_ (W.requestHeaders req) $ \(name, value) -> putBSLn $ CI.original name <> ": " <> value
   app req respond
-
 
 
 opt :: O.Parser Int
@@ -40,3 +44,13 @@ opt = O.option O.auto
 opts = O.info (opt <**> O.helper)
   $ O.fullDesc
   <> O.header "rin - HTTP/HTTPS server for dev/debug"
+
+-- e.g. 2019-08-17T03:33:51.99+0900
+encodeTime :: Ch.Time -> Text
+encodeTime time =
+  Ch.encode_YmdHMSz offsetFormat subsecondPrecision Ch.w3c offsetDatetime
+  where
+    jstOffset          = Ch.Offset (9 * 60)
+    offsetDatetime     = Ch.timeToOffsetDatetime jstOffset time
+    offsetFormat       = Ch.OffsetFormatColonOff
+    subsecondPrecision = Ch.SubsecondPrecisionFixed 2
