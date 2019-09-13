a5518398dfaa1f72e48c9c7d1f814d15ddd3ba79
kamoii <>
2019-09-13 10:52:49 +0900

    Fix release-static.nix

--- nix/release-static.nix
+++ nix/release-static.nix
@@ -1,17 +1,18 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
+  pkgs64 = pkgs.pkgsCross.musl64;
   hsPkgs = import ./default.nix {
-    pkgs = pkgs.pkgsCross.musl64;
+    pkgs = pkgs64;
     additional-modules = [
       { packages.rin.components.exes.rin.configureFlags = [
           "--disable-executable-dynamic"
           "--disable-shared"
           "--ghc-option=-optl=-pthread"
           "--ghc-option=-optl=-static"
-          "--ghc-option=-optl=-L${pkgs.gmp6.override { withStatic = true; }}/lib"
-          "--ghc-option=-optl=-L${pkgs.zlib.static}/lib"
-          "--ghc-option=-optl=-L${pkgs.libffi.overrideAttrs (old: { dontDisableStatic = true; })}/lib"
+          "--ghc-option=-optl=-L${pkgs64.gmp6.override { withStatic = true; }}/lib"
+          "--ghc-option=-optl=-L${pkgs64.zlib.static}/lib"
+          "--ghc-option=-optl=-L${pkgs64.libffi.overrideAttrs (old: { dontDisableStatic = true; })}/lib"
         ];
       }
     ];
